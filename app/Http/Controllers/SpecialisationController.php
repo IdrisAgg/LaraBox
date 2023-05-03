59352462411b7a80e9049bc6efd7ec54402692ef
IdrisAgg <agg.idris@gmail.com>
2023-05-03 16:03:30 +0200

    creation du model specialisation (model/controller)

---
+++ app/Http/Controllers/SpecialisationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\specialisation;
+use Illuminate\Http\Request;
+
+class SpecialisationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(specialisation $specialisation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(specialisation $specialisation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, specialisation $specialisation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(specialisation $specialisation)
+    {
+        //
+    }
+}
