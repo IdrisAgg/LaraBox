e18033cfb9596d42fca3643fbf98bebb0dda88a2
IdrisAgg <agg.idris@gmail.com>
2023-05-05 16:21:53 +0200

    seeders resto update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\restaurent;
 use App\Models\User;
 
 class DatabaseSeeder extends Seeder
@@ -15,10 +16,45 @@ class DatabaseSeeder extends Seeder
     {
         //User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
              'name' => 'Idris',
              'email' => 'idris@gmail.com',
              'password' =>bcrypt("mdpmdpmdp"),
          ]);
+
+         //restaurents::factory(10)->create();
+
+        restaurent::create([
+            'nom' => 'kodawari-Ramen Yokocho',
+            'ville' => 'Paris',
+            'adresse' => '26 rue Mazarine',
+            'horaireDebut' => '12:30',
+            'horaireFin' => '23:30',
+        ]);
+
+        restaurent::create([
+            'nom' => 'kodawari-Ramen Tsukiji',
+            'ville' => 'Paris',
+            'adresse' => '12, rue de Richelieu ',
+            'horaireDebut' => '12:30',
+            'horaireFin' => '23:30',
+        ]);
+
+        restaurent::create([
+            'nom' => 'Kadoya',
+            'ville' => 'Paris',
+            'adresse' => '28 rue Saint-Anne',
+            'horaireDebut' => '12:30',
+            'horaireFin' => '22:30',
+        ]);
+
+        restaurent::create([
+            'nom' => 'Ippudo',
+            'ville' => 'Paris',
+            'adresse' => '76 Rue Jean-Jacques Rousseau',
+            'horaireDebut' => '12:00',
+            'horaireFin' => '22:00',
+        ]);
+
     }
 }
